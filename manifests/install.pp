f5f6d67089ae5d280ec59e99dfe1b11b46ab042d
Nguyen Kim Hiep <nkhiep@tma.com.vn>
2019-04-09 16:05:23 +0700

    add install

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,14 +1,26 @@
 class mariadb::install{
-    file{ "Add_Repo":
+  file{ "add_Repo":
     ensure => present,
     source => 'puppet:///modules/mariadb/add_repo.sh',
     path => '/tmp/add_repo.sh',
     mode  => '0755',
     owner => 'root',
     group => 'root',
-    notify =>  Exec['run_script'],
+    notify =>  Exec['run_add'],
   }
-  exec { 'run_script':
+  exec { 'run_add':
     command => "/bin/bash '/tmp/add_repo.sh'",
+  }
+  file { "install_MariaDB" :
+    ensure => present,
+    source => 'puppet:///modules/mariadb/install_mariaDB.sh',
+    path => '/tmp/install_mariaDB.sh',
+    mode => '0755',
+    owner => 'root',
+    group => 'root',
+    notify => Exec['run_install']
+  }
+  exec { 'run_install'
+    command => "/usr/bin/apt-get '/tmp/install_mariaDB.sh'"
   }
 }
