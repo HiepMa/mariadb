3782ee0757d481a499682a12dc7c3fa563cff1db
Nguyen Kim Hiep <nkhiep@tma.com.vn>
2019-04-09 15:55:07 +0700

    fix add repo

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,3 +1,14 @@
 class mariadb::install{
-  include mariadb::install::add
+    file{ "Add_Repo":
+    ensure => present,
+    source => 'puppet:///modules/mariadb/add_repo.sh',
+    path => '/tmp/add_repo.sh',
+    mode  => '0755',
+    owner => 'root',
+    group => 'root',
+    notify =>  Exec['run_script'],
+  }
+  exec { 'run_script':
+    command => "/bin/bash '/tmp/add_repo.sh'",
+  }
 }
--- manifests/install/add.pp
+++
@@ -1,5 +0,0 @@
-class mariadb::install::add{
-  exec { 'run_script':
-    command => "/bin/bash -c '/etc/puppetlabs/code/environments/production/modules/mariadb/manifests/install/add_repo.sh'",
-  }
-}
