4e40ac1f8660bef2926082882bfcbcd6fa0f4e90
Nguyen Kim Hiep <nkhiep@tma.com.vn>
2019-04-09 15:27:31 +0700

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,14 +1,3 @@
 class mariadb::install{
-  file{ '/vagrant/01_script_main.sh':
-    ensure => present,
-    source => '/vagrant/01_script_main.sh',
-    mode  => '0755',
-    owner => 'root',
-    group => 'root',
-    notify =>  Exec['run_script'],
-  }
-  exec { 'run_script':
-    command => "/bin/bash -c '/vagrant/01_script_main.sh'",
-    refreshonly => true,
-  }
+  include mariadb::install::add
 }
